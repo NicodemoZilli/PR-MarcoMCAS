db4e5a761ecdca8fdf9465d7190b7d595196788c
Nicodemo Zilli <nicozv13@gmail.com>
2021-12-14 21:39:47 -0600

    actualizacion

--- src/metricas/mreuso/mmodularidad/mautonomia/maislamiento/MAislamiento.java
+++ src/metricas/mreuso/mmodularidad/mautonomia/maislamiento/MAislamiento.java
@@ -1,5 +1,7 @@
 package metricas.mreuso.mmodularidad.mautonomia.maislamiento;
 
+import main.Contexto;
+
 /**
  * @author David Nicodemo Zilli Vazquez
  * @version 1.0
@@ -10,10 +12,16 @@ import metricas.mreuso.mmodularidad.mautonomia.MAutonomia;
 
 public class MAislamiento implements MAutonomia{
 
+	private Contexto Ctx;
+	private float MA;
+	
+	public MAislamiento(Contexto Ctx) {
+		this.Ctx = Ctx;
+	}
+	
 	@Override
 	public void Calcular() {
-		// TODO Auto-generated method stub
-		
+		this.MA=0;
 	}
 
 }
